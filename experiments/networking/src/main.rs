964f2b701415314f25d2c4ab3bca0ac1d07c0866
Vidyoot Senthil <vidyoots@gmail.com>
2025-06-02 14:14:46 -0700

    fix: reduce min number of peers in mesh network

--- experiments/networking/src/main.rs
+++ experiments/networking/src/main.rs
@@ -43,12 +43,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 gossipsub::MessageId::from(s.finish().to_string())
             };
 
-            // Set a custom gossipsub configuration
             let gossipsub_config = gossipsub::ConfigBuilder::default()
                 .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
-                // signing)
+                .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
                 .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .mesh_n_low(1) // Minimum number of peers in mesh network (default is 4)
+                .mesh_n_high(12) // Maximum number of peers in mesh network
+                .mesh_n(3) // Target number of peers in mesh network (default is 6)
+                .mesh_outbound_min(1) // Minimum outbound connections (default is 2)
+                .gossip_lazy(3) // Number of peers to gossip to (default is 6)
+                .flood_publish(true) // Always flood publish messages to all peers, regardless of mesh
                 .build()
                 .map_err(io::Error::other)?; // Temporary hack because `build` does not return a proper `std::error::Error`.
 
@@ -65,27 +69,31 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .build();
 
     // Create a Gossipsub topic
-    let topic = gossipsub::IdentTopic::new("test-net");
+    let topic = gossipsub::IdentTopic::new("publish-key");
     // subscribes to our topic
     swarm.behaviour_mut().gossipsub.subscribe(&topic)?;
 
     // Read full lines from stdin
     let mut stdin = io::BufReader::new(io::stdin()).lines();
 
-    // Listen on all interfaces and whatever port the OS assigns
     swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
     println!("Enter messages via STDIN and they will be sent to connected peers using Gossipsub");
+    println!("Network configured with lenient peer requirements to reduce 'insufficient peers' errors");
 
-    // Kick it off
     loop {
         select! {
             Ok(Some(line)) = stdin.next_line() => {
+                let peer_count = swarm.behaviour().gossipsub.all_peers().count();
+                println!("Connected peers: {}", peer_count);
+                
                 if let Err(e) = swarm
                     .behaviour_mut().gossipsub
                     .publish(topic.clone(), line.as_bytes()) {
                     println!("Publish error: {e:?}");
+                } else {
+                    println!("Message published successfully to {} peers", peer_count);
                 }
             }
             event = swarm.select_next_some() => match event {
@@ -109,6 +117,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         "Got message: '{}' with id: {id} from peer: {peer_id}",
                         String::from_utf8_lossy(&message.data),
                     ),
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Subscribed {
+                    peer_id,
+                    topic,
+                })) => {
+                    println!("Peer {peer_id} subscribed to topic {topic}");
+                },
+                SwarmEvent::Behaviour(MyBehaviourEvent::Gossipsub(gossipsub::Event::Unsubscribed {
+                    peer_id,
+                    topic,
+                })) => {
+                    println!("Peer {peer_id} unsubscribed from topic {topic}");
+                },
+                SwarmEvent::ConnectionEstablished { peer_id, .. } => {
+                    println!("Connection established with peer: {peer_id}");
+                    let peer_count = swarm.behaviour().gossipsub.all_peers().count();
+                    println!("Total connected peers: {}", peer_count);
+                },
+                SwarmEvent::ConnectionClosed { peer_id, .. } => {
+                    println!("Connection closed with peer: {peer_id}");
+                    let peer_count = swarm.behaviour().gossipsub.all_peers().count();
+                    println!("Total connected peers: {}", peer_count);
+                },
                 SwarmEvent::NewListenAddr { address, .. } => {
                     println!("Local node is listening on {address}");
                 }
