e1b5b159d3cfd14a6b3d2fcc6b9002a7be8d0e68
cdxker <fdenzell@gmail.com>
2025-06-04 16:05:54 -0700

    feature: add Error type for NetworkCalls

--- node/src/errors.rs
+++ node/src/errors.rs
@@ -1,10 +1,19 @@
 use std::error::Error;
 
 use derive_more::Display;
+use tokio::sync::mpsc::error::SendError;
+
+use crate::swarm_manager::NetworkMessage;
 
 #[derive(Debug, Display)]
 pub enum NodeError {
     Error(String),
+}
+
+#[derive(Debug)]
+pub enum NetworkError {
+    SendError(SendError<NetworkMessage>),
+    RecvError,
 }
 
 impl Error for NodeError {}
