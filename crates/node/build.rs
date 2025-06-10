4bcfe7b39651f6a614e56840c906f3f21c3b4839
Dens Sumesh <dens.sumesh79@gmail.com>
2025-06-10 16:17:35 -0700

    bugfix: fix building with optional protobuf types

--- crates/node/build.rs
+++ crates/node/build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/node.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["proto/node.proto"], &["proto"])?;
+
     Ok(())
 }
