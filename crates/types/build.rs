9db968271bed57ffd2e01675da8c71af18d71f61
Dens Sumesh <dens.sumesh79@gmail.com>
2025-06-17 16:17:47 -0700

    feature: added datatypes for the consensus system

--- crates/types/build.rs
+++ crates/types/build.rs
@@ -1,7 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile_protos(&["proto/grpc.proto", "proto/p2p.proto"], &["proto"])?;
+        .compile_well_known_types(true)
+        .compile_protos(
+            &[
+                "proto/grpc.proto",
+                "proto/p2p.proto",
+                "proto/consensus.proto",
+            ],
+            &["proto"],
+        )?;
 
     Ok(())
 }
