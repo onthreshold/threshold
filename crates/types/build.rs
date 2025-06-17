945c021d2ab7511b227c556ad0814d3bf9718636
Dens Sumesh <dens.sumesh79@gmail.com>
2025-06-17 16:21:56 -0700

    bugfix: fix google types not being found

--- crates/types/build.rs
+++ crates/types/build.rs
@@ -2,6 +2,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_well_known_types(true)
+        .extern_path(".google.protobuf.Timestamp", "::prost_types::Timestamp")
+        .extern_path(".google.protobuf", "::prost_types")
         .compile_protos(
             &[
                 "proto/grpc.proto",
